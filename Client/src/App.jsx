7ea1edc74fff133c85718a282e6e742bc65b2e57
Ali-Alinejad <winford.op@gmail.com>
2024-09-27 12:21:50 +0330

    update Ui

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./normalize.css";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex">
+        <div className="w-[35%] bg-emerald-950 h-[100vh] text-center">
+          <h1 className="text-cyan-50 text-3xl m-10 max-sm:scale-50 max-sm:m-0">
+            Chat AI
+          </h1>
+          <p className="border-b-[1px] m-6 border-white"></p>
+
+          <button
+            className="border-2 p-4 rounded-md m-4 w-[80%] text-white hover:scale-105 transition duration-500 font-semibold max-sm:scale-75 max-sm:font-normal max-sm:w-full max-sm:m-0"
+            aria-label="New Chat"
+          >
+            <span className="animate-pulse">➕</span> چت جدید
+          </button>
+        </div>
+        <div className="w-full bg-slate-300 h-[100vh]">
+          <div>
+            <input
+              className="w-[60%] h-20 rounded-xl shadow-lg ring-2 ring-emerald-400 p-4 absolute bottom-[10%] right-[12%] text-end"
+              type="text"
+              placeholder="سوال خود را بپرسید"
+              aria-label="Ask your question"
+            />
+            <button
+              className="rounded-xl absolute bottom-[13%] right-[7%] scale-[4] max-sm:scale-[2] max-sm:right-[4%] hover:animate-pulse hover:scale-[3.5] transition duration-700"
+              aria-label="Send"
+            >
+              ➡️
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
